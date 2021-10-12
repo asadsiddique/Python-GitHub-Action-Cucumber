dec8d9b27c6081e64714d67337c1dd8d8b002329
asad <asad.siddique712@gmail.com>
2021-10-12 14:22:04 +0500

    init file removed

--- tests/features/steps/tutorialblender.py
+++ tests/features/steps/tutorialblender.py
@@ -1,4 +1,6 @@
-from behave import given, then, when
+from behave   import given, when, then
+from hamcrest import assert_that, equal_to
+from blender  import Blender
 
 @given('I put "{thing}" in a blender')
 def step_given_put_thing_into_blender(context, thing):
@@ -11,4 +13,4 @@ def step_when_switch_blender_on(context):
 
 @then('it should transform into "{other_thing}"')
 def step_then_should_transform_into(context, other_thing):
-    assert(context.blender.result, equal_to(other_thing))
+    assert_that(context.blender.result, equal_to(other_thing))
